189cfe2cfb9a60643249dabe8ba8a9b4933c0e6d
NadezhdaChe <spiral_7@mail.ru>
2015-05-20 14:40:10 +0300

    Создан класс TelephoneFixer

---
+++ src/TelephoneFixer.java
@@ -0,0 +1,43 @@
+/**
+ * Created by Student on 20.05.2015.
+ */
+
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class TelephoneFixer {
+    public static void main(String[] ars) {
+        TelephoneFixer fixer = new TelephoneFixer();
+        fixer.fix(new File("Telephone.txt"));
+    }
+
+    public TelephoneFixer() {
+    }
+
+    public void fix(File file) {
+        String in;
+        try {
+            in = new String(Files.readAllBytes(file.toPath()), "UTF-8");
+        } catch (IOException e) {
+            System.out.println("Couldn't open file to read");
+            return;
+        }
+        Matcher matcher = Pattern.compile("\\s+\\+?(\\d[\\s\\-\\(\\)\\+]{0,3}){11}\\s+").matcher(in);
+        StringBuffer out = new StringBuffer();
+        while (matcher.find()) {
+            String number = matcher.group();
+            number = number.replaceAll("[^\\d]", "");
+            StringBuilder stringBuilder = new StringBuilder(number.replaceFirst("\\d", "1"));
+            number = stringBuilder
+                    .insert(0, " +").insert(3, " ").insert(4, "(").insert(8, ") ").insert(13, "-").insert(16, "-")
+                    .insert(19, " ").toString();
+            matcher.appendReplacement(out, number);
+        }
+        System.out.println(matcher.appendTail(out).toString());
+    }
+}
